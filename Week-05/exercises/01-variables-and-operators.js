87fecca227835429dd045b29fda8e09dd3dcc502
Cristian Zufiaurre <cjzufi@gmail.com>
2023-04-16 19:43:04 -0300

    BaSP-W05-2023: Exercise 1 JS

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,33 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+/* a. Crear dos variables numéricas y utilizar el operador suma para 
+guardar el valor de la suma de ambos números en una 3er variable. */
+console.log('-Exercise 1.a:');
+
+var number1 = 354;
+var number2 = 495;
+
+var sumNumber= number1 + number2;
+
+console.log(sumNumber);
+
+/* b. Crear dos variables de tipo String y concatenarlas guardando 
+el resultado en una 3er variable. */
+console.log('-Exercise 1.b:');
+
+var string1 = 'Java';
+var string2 = 'Script';
+
+var sumString = string1 + string2;
+
+console.log(sumString);
+
+/* c. Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).*/
+console.log('-Exercise 1.c: ');
+
+var string3 = 'periodico';
+var string4 = 'matutino';
+
+var sumLength = string3.length + string4.length;
+
+console.log(sumLength)
